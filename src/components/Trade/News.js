8b0be14c1b0c35082a7d7c2cb7e12dc018a7d2e4
Bretis2019 <mosbahiwalid10@gmail.com>
2023-07-25 10:17:35 +0100

    Made News only display 25 stories on Trade

--- src/components/Trade/News.js
+++ src/components/Trade/News.js
@@ -1,12 +1,12 @@
 import "./News.css"
 
 export default function News(props){
-    const Elements = props.news.map(item => {
+    const Elements = props.news.slice(0,25).map(item => {
         const url  = item.banner_image ? item.banner_image : "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80";
         return (
             <a href={item.url} className={"my-8"}>
                 <div className={"flex gap-x-4"} >
-                    <img src={url} alt={"image"} className={"w-[80px] h-[80px] object-cover rounded-xl border-2 border-black dark:border-white"}/>
+                    <img src={url} alt={""} className={"w-[80px] h-[80px] object-cover rounded-xl border-2 border-black dark:border-white"}/>
                     <div className={"cutoff-text"}>{item.title}</div>
                 </div>
             </a>
